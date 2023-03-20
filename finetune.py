1087cfda410057f7d900b9ded4f0f5566c8f8484
Abu Qader <48742992+aspctu@users.noreply.github.com>
2023-03-19 19:14:34 -0500

    refactoring for readability

--- finetune.py
+++ finetune.py
@@ -1,12 +1,11 @@
+import argparse
 import os
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import torch
 import torch.nn as nn
 import bitsandbytes as bnb
 from datasets import load_dataset
 import transformers
-
 
 from transformers import LlamaForCausalLM, LlamaTokenizer
 from peft import (
@@ -16,56 +15,16 @@ from peft import (
     get_peft_model_state_dict,
 )
 
-
-# adapted for 1xA100 80GB
-MICRO_BATCH_SIZE = 6  
-BATCH_SIZE = 128
-GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
-EPOCHS = 3  # we don't always need 3 tbh
-LEARNING_RATE = 3e-4  # the Karpathy constant
+# hyperparams
 CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
 LORA_R = 8
 LORA_ALPHA = 16
 LORA_DROPOUT = 0.05
 VAL_SET_SIZE = 2000
-# To test everything works, use a smaller dataset 
-# VAL_SET_SIZE = 10
 TARGET_MODULES = [
     "q_proj",
     "v_proj",
 ]
-DATA_PATH = "alpaca_data.json"
-# DATA_PATH = "small_alpaca.json"
-
-model = LlamaForCausalLM.from_pretrained(
-    "decapoda-research/llama-30b-hf",
-    load_in_8bit=True,
-    device_map="auto",
-)
-tokenizer = LlamaTokenizer.from_pretrained(
-    "decapoda-research/llama-30b-hf", add_eos_token=True
-)
-
-model = prepare_model_for_int8_training(model)
-
-config = LoraConfig(
-    r=LORA_R,
-    lora_alpha=LORA_ALPHA,
-    target_modules=TARGET_MODULES,
-    lora_dropout=LORA_DROPOUT,
-    bias="none",
-    task_type="CAUSAL_LM",
-)
-model = get_peft_model(model, config)
-tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
-data = load_dataset("json", data_files=DATA_PATH)
-
-train_val = data["train"].train_test_split(
-    test_size=VAL_SET_SIZE, shuffle=True, seed=42
-)
-train_data = train_val["train"]
-val_data = train_val["test"]
-
 
 def generate_prompt(data_point):
     # sorry about the formatting disaster gotta move fast
@@ -90,7 +49,7 @@ def generate_prompt(data_point):
 {data_point["output"]}"""
 
 
-def tokenize(prompt):
+def tokenize(prompt, tokenizer):
     # there's probably a way to do this with the tokenizer settings
     # but again, gotta move fast
     result = tokenizer(
@@ -103,43 +62,118 @@ def tokenize(prompt):
         "input_ids": result["input_ids"][:-1],
         "attention_mask": result["attention_mask"][:-1],
     }
+    
 
+def train(
+    data_path: str,
+    micro_batch_size: int,
+    batch_size: int,
+    warmup_steps: int,
+    lr: float,
+    epochs: int,
+    report_to: str = "none",
+    logging_steps: int = 20,
+    eval_steps: int = 200,
+    save_steps: int = 200,
+    model_pretrained_name: str = "decapoda-research/llama-30b-hf",
+    output_dir: str = "lora-alpaca",
+):
+    model = LlamaForCausalLM.from_pretrained(
+        model_pretrained_name,
+        load_in_8bit=True,
+        device_map="auto",
+    )
+    tokenizer = LlamaTokenizer.from_pretrained(
+        model_pretrained_name, add_eos_token=True
+    )
 
-train_data = train_data.shuffle().map(lambda x: tokenize(generate_prompt(x)))
-val_data = val_data.shuffle().map(lambda x: tokenize(generate_prompt(x)))
+    model = prepare_model_for_int8_training(model)
 
-trainer = transformers.Trainer(
-    model=model,
-    train_dataset=train_data,
-    eval_dataset=val_data,
-    args=transformers.TrainingArguments(
-        per_device_train_batch_size=MICRO_BATCH_SIZE,
-        gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
-        warmup_steps=100,
-        num_train_epochs=EPOCHS,
-        learning_rate=LEARNING_RATE,
-        fp16=True,
-        logging_steps=20,
-        evaluation_strategy="steps",
-        save_strategy="steps",
-        eval_steps=200,
-        save_steps=200,
-        output_dir="lora-alpaca",
-        report_to="wandb",
-        save_total_limit=3,
-        load_best_model_at_end=True,
-    ),
-    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
-)
-model.config.use_cache = False
+    config = LoraConfig(
+        r=LORA_R,
+        lora_alpha=LORA_ALPHA,
+        target_modules=TARGET_MODULES,
+        lora_dropout=LORA_DROPOUT,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    model = get_peft_model(model, config)
+    tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+    data = load_dataset("json", data_files=data_path)
 
-old_state_dict = model.state_dict
-model.state_dict = (
-    lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
-).__get__(model, type(model))
+    train_val = data["train"].train_test_split(
+        test_size=VAL_SET_SIZE, shuffle=True, seed=42
+    )
+    train_data = train_val["train"]
+    val_data = train_val["test"]
+    
+        
+    train_data = train_data.shuffle().map(lambda x: tokenize(generate_prompt(x), tokenizer))
+    val_data = val_data.shuffle().map(lambda x: tokenize(generate_prompt(x), tokenizer))
 
-trainer.train()
+    gradient_accumulation_steps = batch_size // micro_batch_size
 
-model.save_pretrained("lora-alpaca")
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=micro_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            warmup_steps=warmup_steps,
+            num_train_epochs=epochs,
+            learning_rate=lr,
+            fp16=True,
+            logging_steps=logging_steps,
+            evaluation_strategy="steps",
+            save_strategy="steps",
+            eval_steps=eval_steps,
+            save_steps=save_steps,
+            output_dir=output_dir,
+            report_to=report_to if report_to else "none",
+            save_total_limit=3,
+            load_best_model_at_end=True,
+        ),
+        data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+    )
+    model.config.use_cache = False
 
-print("\n If there's a warning about missing keys above, please disregard :)")
+    old_state_dict = model.state_dict
+    model.state_dict = (
+        lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+    ).__get__(model, type(model))
+
+    trainer.train()
+    
+    model.save_pretrained(output_dir)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, default="alpaca_data.json")
+    parser.add_argument("--micro_batch_size", type=int, default=6)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--warmup_steps", type=int, default=100)
+    parser.add_argument("--lr", type=float, default=3e-4)  
+    parser.add_argument("--epochs", type=int, default=3) 
+    parser.add_argument("--report_to", type=str, default="wandb") 
+    parser.add_argument("--logging_steps", type=int, default=20)
+    parser.add_argument("--eval_steps", type=int, default=200)
+    parser.add_argument("--save_steps", type=int, default=200) 
+    parser.add_argument("--model_pretrained_name", type=str, default="decapoda-research/llama-7b-hf")
+    parser.add_argument("--output_dir", type=str, default="lora-alpaca")
+    args = parser.parse_args()
+    
+    train(
+        data_path=args.data_path,
+        micro_batch_size=args.micro_batch_size,
+        batch_size=args.batch_size,
+        warmup_steps=args.warmup_steps,
+        lr=args.lr,
+        epochs=args.epochs,
+        report_to=args.report_to,
+        logging_steps=args.logging_steps,
+        eval_steps=args.eval_steps,
+        save_steps=args.save_steps,
+        model_pretrained_name=args.model_pretrained_name,
+        output_dir=args.output_dir,
+    )
